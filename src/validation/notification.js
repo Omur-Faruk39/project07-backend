56018ebea1ac57f98b11bdd5bb30838f56ea2648
omur faruk <omurfaruk39bd@gmail.com>
2026-05-04 18:52:32 +0400

    update

--- src/validation/notification.js
+++ src/validation/notification.js
@@ -4,13 +4,14 @@ const validationAddNotification = Joi.object({
   userName: Joi.string().max(50).required(),
   type: Joi.string().valid("friend", "room pass").optional(),
   title: Joi.string().max(500).required(),
-  userName2: Joi.string() // Conditional validation for user_name_2
+  userName2: Joi.string()
     .max(50)
     .when("type", {
       is: Joi.valid("friend"),
       then: Joi.required(),
-      otherwise: Joi.forbidden(), // disallow for other types
+      otherwise: Joi.forbidden(),
     }),
+  data: Joi.string(), // Conditional validation for data
 });
 
 module.exports = {
