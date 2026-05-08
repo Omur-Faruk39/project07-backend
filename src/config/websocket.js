8a75fc20c5b5451e55a9b85a9f3fd44ba40b1247
omur faruk <omurfaruk39bd@gmail.com>
2026-05-08 15:03:18 +0400

    update

---
+++ src/config/websocket.js
@@ -0,0 +1,29 @@
+const { Server } = require("socket.io");
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// store online users
+const onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  // user sends username after connect
+  socket.on("register", (username) => {
+    onlineUsers.set(username, socket.id);
+  });
+
+  socket.on("disconnect", () => {
+    for (let [key, value] of onlineUsers.entries()) {
+      if (value === socket.id) {
+        onlineUsers.delete(key);
+      }
+    }
+  });
+});
+
+module.exports = { io, onlineUsers };
