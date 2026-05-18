124527db83804ece042f615b60fd4791bf2c196e
omur faruk <omurfaruk39bd@gmail.com>
2026-05-18 22:59:36 +0600

    update

--- src/utils/sanitize.js
+++ src/utils/sanitize.js
@@ -23,7 +23,6 @@ function sanitizeInput(data) {
 
     return sanitizedObj;
   }
-  //for string inputs, we will sanitize and check for suspicious JS patterns
 
   if (typeof data === "string") {
     const clean = sanitizeHtml(data, {
